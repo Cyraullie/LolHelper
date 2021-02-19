a84f39bed6d2b1a4916bf427eafba19d783a3c23
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2021-02-19 18:16:18 +0100

    add config file

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 # main.py
 
 import os
-
-
 from discord.ext import commands
 import discord
+from dotenv import load_dotenv
 
-TOKEN = 'NzI1NzczMTE0MTg5MDIxMzI0.XvTm4g.txkPRzX2Q6R5J-iPUxBEzermQNs'
-
+load_dotenv(dotenv_path="config")
 bot = commands.Bot(command_prefix = "*", description = "test de merde")
+
+@bot.event
+async def on_ready():
+    print('Le bot est prêt')
+    print('------')
+
+
 
 @bot.command()
 async def coucou(ctx):
     await ctx.send("Coucou <@&725785724741484585> !")
-    
-    
-@bot.command()
+
 async def serverInfo(ctx):
     server = ctx.guild
     numberOfTextChannels = len(server.text_channels)
@@ -27,23 +30,15 @@ async def serverInfo(ctx):
     
     await ctx.send(message)
 
-@bot.command()
 async def recherche(ctx, NbPersonne, Attente):
 
     await ctx.send(f"Yo <@&725785724741484585>, je suis avec {int(NbPersonne)} personnes et nous vous attendrons pendant {int(Attente)} minutes votre message alors bougez-vous !")
 
-    
-@bot.command()
 async def jarrive(ctx, Heure, Minutes):
 
     await ctx.send(f"Yo <@&725785724741484585>, j'arrive vers {int(Heure)} h {int(Minutes)} !")
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
 
 
-bot.run(TOKEN)
+
+bot.run(os.getenv("TOKEN"))
